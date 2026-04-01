1f791cac1a9ea0fe8d71bfa8c4decfbcb906d57a
Boopeshsr <srboopesh2@gmail.com>
2026-04-01 22:12:51 +0530

    changes

--- Prob10/Solution.java
+++ Prob10/Solution.java
@@ -13,12 +13,7 @@ import static java.util.stream.Collectors.toList;
 
 class Result {
 
-    /*
-     * Complete the 'birthdayCakeCandles' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts INTEGER_ARRAY candles as parameter.
-     */
+
 
     public static int birthdayCakeCandles(List<Integer> candles) {
     int max = 0;
